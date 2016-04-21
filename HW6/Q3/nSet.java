f477c37fa274ec5559f315aac2e2faf7bb1fc7c5
goggalor1954 <goggalor1954@gmail.com>
2016-04-21 15:48:24 -0400

    Started assignment 6
    Started assignment 6

---
+++ HW6/Q3/nSet.java
@@ -0,0 +1,13 @@
+//James roesemann
+//Assignment 6 Question 1
+
+import java.util.*;
+
+public class nSet{
+	public static void main(String [] args){
+		nSet Go = new StoreCredit();
+		Go.go();
+	}
+	public void go(){
+	}
+}
